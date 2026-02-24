8b429134c79efbdb5f51b6cf1a658ac6103aa3a4
Krishna Baviskar <krishnabaviskar40@gmail.com>
2026-02-24 13:20:31 +0530

    Update ChartContainer.tsx

--- src/components/RoleBoard/ChartContainer.tsx
+++ src/components/RoleBoard/ChartContainer.tsx
@@ -1,5 +1,5 @@
 
-"use client";
+"use client"; 
 
 import React, { useState, useEffect } from "react";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
