ab508754f1640cd1f376824564a7a832438b48f9
Krishna Baviskar <krishnabaviskar40@gmail.com>
2026-02-24 13:20:38 +0530

    Update DataPanel.tsx

--- src/components/RoleBoard/DataPanel.tsx
+++ src/components/RoleBoard/DataPanel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"; 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
